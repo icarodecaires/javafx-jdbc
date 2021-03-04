792512b4f74a29eab54f8876b794ae27b087407c
Icaro Caires <icaro_de_caires@outlook.com>
2021-03-03 21:01:14 -0400

    Commit 2

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,23 +1,33 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
 
-public class DepartmentListController implements Initializable {
+public class DepartmentListController implements Initializable, DataChangeListener {
 
 	private DepartmentService service;
 	
@@ -42,8 +52,10 @@ public class DepartmentListController implements Initializable {
 	}
 	
 	@FXML
-	public void onBtNewAction() {
-		System.out.println("onBtNewAction");
+	public void onBtNewAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		Department obj = new Department();
+		createDialogForm(obj,"/gui/DepartmentForm.fxml",parentStage);
 	}
 	
 	@Override
@@ -73,6 +85,37 @@ public class DepartmentListController implements Initializable {
 		obsList = FXCollections.observableArrayList(list);
 		
 		tableViewDepartment.setItems(obsList);
+	}
+	
+	private void createDialogForm(Department obj, String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+
+			DepartmentFormController controller = loader.getController();
+			controller.setDepartment(obj);
+			controller.setDepartmentService(new DepartmentService());
+			controller.subscribeDataChageListener(this);
+			controller.updateFormData();
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Entre com os dados do Departamento");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+			
+		}
+		catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Erro ao Carregar View", e.getMessage(), AlertType.ERROR);
+		}
+	}
+
+	@Override
+	public void onDataChanged() {
+		updateTableView();
+		
 	}
 
 }
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,9 +1,9 @@
 package gui;
 
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -18,97 +18,68 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import model.services.DepartmentService;
 
-public class MainViewController implements Initializable{
-	
+public class MainViewController implements Initializable {
+
 	@FXML
 	private MenuItem menuItemSeller, menuItemDepartment, menuItemAbout;
-	
-	
-	
+
 	@FXML
 	public void onMenuItemSellerAction() {
 		System.out.println("onMenuItemSellerAction");
 	}
-	
+
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+		});
 	}
-	
+
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml",x -> {});
 	}
-	
-	
-	
-	
+
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	//synchronized garante que o processamento não será interrompido no multtread
-	
-	private synchronized void loadView(String absoluteName) {
-		
+
+	// synchronized garante que o processamento não será interrompido no multtread
+
+	private synchronized <T> void  loadView(String absoluteName, Consumer<T> initializingAction) {
+
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			//Filhos do novo Vbox
+			// Filhos do novo Vbox
 			VBox newVbox = loader.load();
-			
+
 			Scene mainScene = Main.getMainScene();
-			//Pegando Uma Referencia do Vbox Principal
+			// Pegando Uma Referencia do Vbox Principal
 			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-			
-			//Pegando Uma referencia do menu
+
+			// Pegando Uma referencia do menu
 			Node mainMenu = mainVBox.getChildren().get(0);
-			
-			//Limpando todos os filhos do Vbox Principal
+
+			// Limpando todos os filhos do Vbox Principal
 			mainVBox.getChildren().clear();
-			
-			//Adicionando o menu e o novo Vbox ao Vbox principal
+
+			// Adicionando o menu e o novo Vbox ao Vbox principal
 			mainVBox.getChildren().add(mainMenu);
 			mainVBox.getChildren().addAll(newVbox.getChildren());
 			
-		}
-		catch (IOException e) {
+			
+			//executa a função lambda passada por parametro para carregar a tableview
+			T controller = loader.getController();
+			initializingAction.accept(controller);
+			
+
+		} catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Erro ao Carregar a Pagina", e.getMessage(), AlertType.ERROR);
 		}
 	}
-	
-	
-private synchronized void loadView2(String absoluteName) {
-		
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			//Filhos do novo Vbox
-			VBox newVbox = loader.load();
-			
-			Scene mainScene = Main.getMainScene();
-			//Pegando Uma Referencia do Vbox Principal
-			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-			
-			//Pegando Uma referencia do menu
-			Node mainMenu = mainVBox.getChildren().get(0);
-			
-			//Limpando todos os filhos do Vbox Principal
-			mainVBox.getChildren().clear();
-			
-			//Adicionando o menu e o novo Vbox ao Vbox principal
-			mainVBox.getChildren().add(mainMenu);
-			mainVBox.getChildren().addAll(newVbox.getChildren());
-			
-			//pegando uma referencia para o controller da view
-			DepartmentListController controller = loader.getController();
-			controller.setDepartmentService(new DepartmentService());
-			controller.updateTableView();
-			
-		}
-		catch (IOException e) {
-			Alerts.showAlert("IO Exception", "Erro ao Carregar a Pagina", e.getMessage(), AlertType.ERROR);
-		}
-	}
+
 
 }
